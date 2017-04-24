c96ce597bc65bbffce5709f135a723dd6346ebbd
Adam Salvo <adsalvo95@gmail.com>
2017-04-24 00:03:31 -0600

    Calculation rewrite (js2) complete with unit-test2 setup.

--- js2/car-insurance.js
+++
@@ -1,8 +0,0 @@
-// let carInsurance6Mo = function() {
-//     return (numCars() === 1 ? car_insurance_avg_per_mo_single : car_insurance_avg_per_mo_married * 2);
-// };
-//
-// let numberOfCars = function () {
-//     return numCars();
-// };
-
