094ad12c9cda3058be321902dc1c8c72b4373b64
Adam Salvo <adsalvo95@gmail.com>
2017-04-24 18:29:17 -0600

    Added default value comments to user-inputs

--- js/user-inputs.js
+++ js/user-inputs.js
@@ -1,28 +1,28 @@
 // Family Composition --------------------------------------------------------------------------------------------------
 // Total of these cannot be > 8.
-let num_adults        = 0; // 'Family Composition'!G4
-let num_infants       = 0; // 'Family Composition'!G7
-let num_preschoolers  = 0; // 'Family Composition'!G10
-let num_schoolagers   = 0; // 'Family Composition'!G13
-let num_teenagers     = 0; // 'Family Composition'!G16
+let num_adults        = 1; // 'Family Composition'!G4 - Default 1
+let num_infants       = 0; // 'Family Composition'!G7 - Default 0
+let num_preschoolers  = 0; // 'Family Composition'!G10 - Default 0
+let num_schoolagers   = 0; // 'Family Composition'!G13 - Default 0
+let num_teenagers     = 0; // 'Family Composition'!G16 - Default 0
 
 // ChangeChildcare  ----------------------------------------------------------------------------------------------------
-let estimated_baby_sitting_annual   = 0;    // 'ChangeChildcare'!E13
-let use_childcare                   = "No"; // 'ChangeChildcare'!F17 - "No"/"Yes"
-let use_family_care                 = "No"; // 'ChangeChildcare'!I13 - "No"/"Yes"
+let estimated_baby_sitting_annual   = 0;    // 'ChangeChildcare'!E13 - Default 0
+let use_childcare                   = "Yes"; // 'ChangeChildcare'!F17 - "No"/"Yes" - Default "Yes"
+let use_family_care                 = "No"; // 'ChangeChildcare'!I13 - "No"/"Yes" - Default "No"
 
 // ChangeHousing  ------------------------------------------------------------------------------------------------------
-let rooms = "Standard"; // 'ChangeHousing'!E15 - "Standard"/1/2/3/4
+let rooms = "Standard"; // 'ChangeHousing'!E15 - "Standard"/1/2/3/4 - Default "Standard"
 
 // MarketplaceHealthcare -----------------------------------------------------------------------------------------------
-let marketplace_healthcare = "No";  // 'MarketplaceHealthcare'!F16 - "No"/"Yes"
+let marketplace_healthcare = "No";  // 'MarketplaceHealthcare'!F16 - "No"/"Yes" - Default "No"
 
 // Number of Cars ------------------------------------------------------------------------------------------------------
-let cars = "Standard";  // 'Number of Cars'H13 - "Standard"/0/1/2/3/4/5/6
+let cars = "Standard";  // 'Number of Cars'H13 - "Standard"/0/1/2/3/4/5/6 - Default "Standard"
 
 // PublicTransportation ------------------------------------------------------------------------------------------------
-let bus_passes_adult = 0;   // 'PublicTransportation'!G16 - 0/1/2/3/4/5
-let bus_passes_child = 0;   // 'PublicTransportation'!K16 - 0/1/2/3/4/5/6/7/8
+let bus_passes_adult = 0;   // 'PublicTransportation'!G16 - 0/1/2/3/4/5 - Default 0
+let bus_passes_child = 0;   // 'PublicTransportation'!K16 - 0/1/2/3/4/5/6/7/8 - Default
 
 function get_user_inputs_from_local_storage(){
     if (localStorage.getItem("num_adults")){
