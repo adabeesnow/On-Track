89eba85c93291ba32084fb303a984b8d83aec629
Friff14 <friffsockpuppet@gmail.com>
2017-04-30 17:39:05 -0600

    some bug fixes

--- form/form.js
+++ form/form.js
@@ -86,129 +86,8 @@ const getParams = query => {
 
 console.log("form.js loaded.");
 
-let adults;
-let infants;
-let preschoolers;
-let schoolagers;
-let teenagers;
 
-function verifyInputs(){
-    if ($("#number-of-adults").val() == "") {
-        adults = 1;
-    } else {
-        adults = parseInt($("#number-of-adults").val());
-    }
-    if ($("#number-of-infants").val() == "") {
-        infants = 0;
-    } else {
-        infants = parseInt($("#number-of-infants").val());
-    }
-    if ($("#number-of-preschoolers").val() == "") {
-        preschoolers = 0;
-    } else {
-        preschoolers = parseInt($("#number-of-preschoolers").val());
-    }
-    if ($("#number-of-schoolagers").val() == "") {
-        schoolagers = 0;
-    } else {
-        schoolagers = parseInt($("#number-of-schoolagers").val());
-    }
-    if ($("#number-of-teenagers").val() == "") {
-        teenagers = 0;
-    } else {
-        teenagers = parseInt($("#number-of-teenagers").val());
-    }
-    // let adults = parseInt($("#number-of-adults").val());
-    // let infants = parseInt($("#number-of-infants").val());
-    // let preschoolers = parseInt($("#number-of-preschoolers").val());
-    // let schoolagers = parseInt($("#number-of-schoolagers").val());
-    // let teenagers = parseInt($("#number-of-teenagers").val());
 
-    localStorage.num_adults = adults;
-    localStorage.num_infants = infants;
-    localStorage.num_preschoolers = preschoolers;
-    localStorage.num_schoolagers = schoolagers;
-    localStorage.num_teenagers = teenagers;
-
-    // standard = 1 car per adult
-    localStorage.cars = localStorage.num_adults;
-
-    // standard = 1 room for all adults, and 1 room per 2 kids
-    let num_kids = parseInt(localStorage.num_infants)
-        + parseInt(localStorage.num_preschoolers)
-        + parseInt(localStorage.num_schoolagers)
-        + parseInt(localStorage.num_teenagers);
-    console.log("num kidsSSSSS", num_kids);
-    let num_kid_rooms;
-    // up to 2 kids per room
-    if (num_kids <= 6) { // excel app only takes up to 8 family members
-        num_kid_rooms = Math.ceil(num_kids / 2);
-    } else {
-        num_kid_rooms = 3
-    } // excel app only goes up to 4 rooms
-    console.log("num kid rooms", num_kid_rooms);
-    // adult bedrooms is always 1 in the excel app
-    localStorage.rooms = (1 + num_kid_rooms);
-    localStorage.estimated_baby_sitting_annual = estimated_baby_sitting_annual;
-    if (infants + preschoolers + schoolagers + teenagers > 0) {
-        localStorage.use_childcare = "Yes";
-    } else {
-        localStorage.use_childcare = "No";
-    }
-
-    localStorage.use_family_care = use_family_care;
-    localStorage.marketplace_healthcare = marketplace_healthcare;
-    localStorage.bus_passes_adult = bus_passes_adult;
-    localStorage.bus_passes_child = bus_passes_child;
-
-}
-function verifyInputsAndStepNext() {
-    if ((adults + infants + preschoolers + schoolagers + teenagers ) > 8) {
-        alert("This form is only accurate for families of up to 8 members.");
-        return false;
-    }
-    else {
-        wizard.steps('next');
-    }
-
-}
-
-function calculate_input_data() {
-    let adults;
-    let infants;
-    let preschoolers;
-    let schoolagers;
-    let teenagers;
-    if ($("#number-of-adults").val() == "") {
-        adults = 1;
-    } else {
-        adults = parseInt($("#number-of-adults").val());
-    }
-    if ($("#number-of-infants").val() == "") {
-        infants = 0;
-    } else {
-        infants = parseInt($("#number-of-infants").val());
-    }
-    if ($("#number-of-preschoolers").val() == "") {
-        preschoolers = 0;
-    } else {
-        preschoolers = parseInt($("#number-of-preschoolers").val());
-    }
-    if ($("#number-of-schoolagers").val() == "") {
-        schoolagers = 0;
-    } else {
-        schoolagers = parseInt($("#number-of-schoolagers").val());
-    }
-    if ($("#number-of-teenagers").val() == "") {
-        teenagers = 0;
-    } else {
-        teenagers = parseInt($("#number-of-teenagers").val());
-    }
-    // let adults = parseInt($("#number-of-adults").val());
-    // let infants = parseInt($("#number-of-infants").val());
-    // let preschoolers = parseInt($("#number-of-preschoolers").val());
-    // let schoolagers = parseInt($("#number-of-schoolagers").val());
-    // let teenagers = parseInt($("#number-of-teenagers").val());
 function calculate_input_data(){
     let adults = parseInt($("#number-of-adults").val());
     let infants = parseInt($("#number-of-infants").val());
@@ -254,7 +133,19 @@ function calculate_input_data(){
 }
 
 function store_input_data() {
-    verifyInputs();
+    localStorage.num_adults = $("#number-of-adults").val();
+    localStorage.num_infants = $("#number-of-infants").val();
+    localStorage.num_preschoolers = $("#number-of-preschoolers").val();
+    localStorage.num_schoolagers = $("#number-of-schoolagers").val();
+    localStorage.num_teenagers = $("#number-of-teenagers").val();
+    localStorage.estimated_baby_sitting_annual = parseFloat($("#babysitting-costs").val()) * 12;
+    localStorage.use_childcare = $("#child-care-needed").val();
+    localStorage.use_family_care = $("#family-care").val();
+    localStorage.rooms = $("#number-of-rooms").val();
+    localStorage.marketplace_healthcare = $("#marketplace-healthcare").val();
+    localStorage.cars = $("#cars").val();
+    localStorage.bus_passes_adult = $("#adult-passes").val();
+    localStorage.bus_passes_child = $("#child-passes").val();
     window.location.href = 'results.html';
 }
 
